400a3d5fea4b5b3bf9c64087833c954ac48e8322
Phong Trương <ttphong.a5@gmail.com>
2021-12-06 15:31:57 +0700

    update data

--- BT Week2.sql
+++ BT Week2.sql
@@ -1,5 +1,5 @@
-create database test
-use test
+create database Test
+use Test
 CREATE TABLE customers(
 customerid int,
 firstname varchar(255),
@@ -15,3 +15,27 @@ item varchar(255),
 quantity int,
 price float,
 );
+
+
+INSERT INTO items_ordered (customerid, order_date, item, quantity, price)
+VALUES ('10438', '1999-11-02', 'Pillow', 1, 8.50);
+INSERT INTO items_ordered (customerid, order_date, item, quantity, price)
+VALUES ('10298', '1999-12-01', 'Helmet', 1, 22.00);
+INSERT INTO items_ordered (customerid, order_date, item, quantity, price)
+VALUES ('10499', '1999-12-15', 'Bicycle', 1, 380.50);
+INSERT INTO items_ordered (customerid, order_date, item, quantity, price)
+VALUES ('10449', '1999-12-22', 'Canoe', 1, 280.00);
+INSERT INTO items_ordered (customerid, order_date, item, quantity, price)
+VALUES ('10101', '1999-12-30', 'Hoola Hoop', 3, 14.75);
+INSERT INTO items_ordered (customerid, order_date, item, quantity, price)
+VALUES ('10330', '2000-01-01', 'Flashlight', 4, 28.00);
+INSERT INTO items_ordered (customerid, order_date, item, quantity, price)
+VALUES ('10101', '2000-01-02', 'Lantern', 1, 16.00);
+INSERT INTO items_ordered (customerid, order_date, item, quantity, price)
+VALUES ('10298', '2000-03-18', 'Pocket Knife', 1, 22.38);
+INSERT INTO items_ordered (customerid, order_date, item, quantity, price)
+VALUES ('10449', '2000-03-19', 'Canoe paddle', 2, 40.00);
+INSERT INTO items_ordered (customerid, order_date, item, quantity, price)
+VALUES ('10298', '2000-04-01', 'Ear Muffs', 1, 12.50);
+INSERT INTO items_ordered (customerid, order_date, item, quantity, price)
+VALUES ('10330', '2000-04-19', 'Shovel', 1, 16.75);
